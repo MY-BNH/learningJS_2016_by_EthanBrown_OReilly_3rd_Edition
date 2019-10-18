c46121523f7f2234e1d8cb97adcf859eea4d8f72
mybnh <mybnhdm30mt@yahoo.com>
2019-10-18 15:25:27 +0330

    pp First commit

---
+++ chapter_01/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+	'use strict';
+	paper.install(window);
+	paper.setup(document.getElementById('mainCanvas'));
+
+	//	var c = Shape.Circle(200, 200, 50);
+	//	c.fillColor = 'green';
+	/* ---------------------------------------------------- */
+
+	//	var c;
+	//	for (var x = 25; x < 400; x += 50) {
+	//		for (var y = 25; y < 400; y += 50) {
+	//			c = Shape.Circle(x, y, 20);
+	//			c.fillColor = 'green';
+	//		}
+	//	}
+	/* ---------------------------------------------------- */
+
+//		let tool = new Tool();
+//		tool.onMouseDown = function (event) {
+//	//		let c = Shape.Circle(event.point.x, event.point.y, 20);
+//			let c = Shape.Circle(event.point, 20);
+//			c.fillColor = 'green';
+//		};
+	/* ---------------------------------------------------- */
+
+	var c = Shape.Circle(200, 200, 80);
+	c.fillColor = 'black';
+	var text = new PointText(200, 200);
+	text.justification = 'center';
+	text.fillColor = 'white';
+	text.fontSize = 20;
+	text.content = 'hello world';
+
+
+	paper.view.draw();
+});
